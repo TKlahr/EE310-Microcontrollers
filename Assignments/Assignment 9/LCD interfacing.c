a386ec008fb2a5ae99ca11e8a5c7718df84edd24
TKlahr <152762116+TKlahr@users.noreply.github.com>
2026-05-06 16:40:45 -0700

    Refactor LCD interfacing code and improve structure

--- Assignments/Assignment 9/LCD interfacing.c
+++ Assignments/Assignment 9/LCD interfacing.c
@@ -21,218 +21,270 @@
 #include <xc.h>
 #include <stdio.h>
 
-// CONFIG
+#define _XTAL_FREQ 4000000UL
+
 #pragma config FEXTOSC = OFF
 #pragma config RSTOSC = HFINTOSC_1MHZ
 #pragma config CLKOUTEN = OFF
+#pragma config WDTE = OFF
+#pragma config LVP = OFF
 #pragma config MCLRE = EXTMCLR
-#pragma config LVP = ON
-#pragma config MVECEN = OFF
+#pragma config MVECEN = ON
 
-#define _XTAL_FREQ 4000000
+#define RS LATDbits.LATD0
+#define EN LATDbits.LATD1
+#define LCD_DATA LATB
 
-// LCD pins
-#define RS LATD0
-#define EN LATD1
-#define ldata LATB
+#define RED_LED LATE0
 
-#define LCD_Port TRISB
-#define LCD_Control TRISD
+volatile unsigned char haltRequest = 0;
 
-// Prototypes
-void LCD_Init(void);
-void LCD_Command(char);
-void LCD_Char(char);
-void LCD_String(const char *);
-void LCD_String_xy(char, char, const char *);
-void LCD_Clear(void);
-void MSdelay(unsigned int);
-
-void ADC_Init(void);
-unsigned int ADC_Read(void);
-void Display_Voltage(void);
-
-void IOC_Init(void);
-void __interrupt() ISR(void);
-
-//-------------------- MAIN --------------------
-void main(void)
+void delay_ms(unsigned int ms)
 {
-    // LCD digital
-    ANSELB = 0x00;
-    ANSELD = 0x00;
+    while(ms--) __delay_ms(1);
+}
 
-    // ADC input RA1
+void LCD_Enable()
+{
+    EN = 1;
+    delay_ms(2);
+    EN = 0;
+    delay_ms(2);
+}
+
+void LCD_Command(unsigned char cmd)
+{
+    RS = 0;
+    LCD_DATA = cmd;
+    LCD_Enable();
+}
+
+void LCD_Char(unsigned char data)
+{
+    RS = 1;
+    LCD_DATA = data;
+    LCD_Enable();
+}
+
+void LCD_String(const char *msg)
+{
+    while(*msg) LCD_Char(*msg++);
+}
+
+void LCD_SetCursor(unsigned char row, unsigned char col)
+{
+    if(row == 1)
+        LCD_Command(0x80 + col);
+    else
+        LCD_Command(0xC0 + col);
+}
+
+void LCD_Clear()
+{
+    LCD_Command(0x01);
+    delay_ms(10);
+}
+
+void LCD_Init()
+{
+    delay_ms(50);
+
+    LCD_DATA = 0x30; LCD_Enable(); delay_ms(10);
+    LCD_DATA = 0x30; LCD_Enable(); delay_ms(10);
+    LCD_DATA = 0x30; LCD_Enable(); delay_ms(10);
+
+    LCD_Command(0x38);
+    LCD_Command(0x08);
+    LCD_Command(0x01);
+    LCD_Command(0x06);
+    LCD_Command(0x0C);
+}
+
+void ADC_Init()
+{
     TRISAbits.TRISA1 = 1;
     ANSELAbits.ANSELA1 = 1;
 
-    LATB = 0x00;
-    LATD = 0x00;
-
-    LCD_Init();
-    LCD_Clear();
-
-    ADC_Init();
-    IOC_Init();
-
-    while(1)
-    {
-        Display_Voltage();
-        MSdelay(500);
-    }
-}
-
-//-------------------- ADC --------------------
-void ADC_Init(void)
-{
-    ADCON0bits.ADFM = 1;
-    ADCON0bits.CS = 1;
     ADPCH = 0x01;
-    ADCON0bits.ADON = 1;
+
+    ADREFbits.ADPREF = 0;
+    ADREFbits.ADNREF = 0;
+
+    ADCLK = 0x3F;
+    ADACQ = 0x3F;
+
+    ADCON0bits.FM = 1;
+    ADCON0bits.ON = 1;
 }
 
-unsigned int ADC_Read(void)
+unsigned int ADC_Read()
 {
+    ADPCH = 0x01;
+    delay_ms(1);
+
     ADCON0bits.GO = 1;
     while(ADCON0bits.GO);
 
     return ((unsigned int)ADRESH << 8) | ADRESL;
 }
 
-//-------------------- DISPLAY --------------------
-void Display_Voltage(void)
-{
-    unsigned int adcValue;
-    float voltage;
-    char buffer[16];
-    char *soundLevel;
-
-    adcValue = ADC_Read();
-    voltage = ((float)adcValue / 4095.0) * 5.0;
-
-    if (voltage < 1.5)
-        soundLevel = "quiet";
-    else if (voltage < 2.5)
-        soundLevel = "normal";
-    else if (voltage < 3.5)
-        soundLevel = "loud";
-    else
-        soundLevel = "obnox";
-
-    LCD_String_xy(1, 0, "Sound:        ");
-    LCD_String_xy(1, 7, soundLevel);
-
-    sprintf(buffer, "%.2fV Level   ", voltage);
-    LCD_String_xy(2, 0, buffer);
-}
-
-//-------------------- INTERRUPT --------------------
 void IOC_Init(void)
 {
-    // RC1 input (button)
-    TRISCbits.TRISC1 = 1;
-    ANSELCbits.ANSELC1 = 0;
+    ANSELC = 0x00;
+    ANSELE = 0x00;
 
-    // RE0 output (LED)
-    TRISEbits.TRISE0 = 0;
-    ANSELEbits.ANSELE0 = 0;
-    LATEbits.LATE0 = 0;
+    TRISE0 = 0;
+    TRISC2 = 1;
 
-    // IOC setup
-    IOCCPbits.IOCCP1 = 1;
-    IOCCNbits.IOCCN1 = 0;
-    IOCCFbits.IOCCF1 = 0;
+    RED_LED = 0;
 
-    PIE0bits.IOCIE = 1;
+    WPUCbits.WPUC2 = 1;
+
+    IOCCNbits.IOCCN2 = 1;
+    IOCCPbits.IOCCP2 = 0;
+
+    IOCCFbits.IOCCF2 = 0;
     PIR0bits.IOCIF = 0;
 
+    PIE0bits.IOCIE = 1;
+
+    INTCON0bits.IPEN = 0;
     INTCON0bits.GIE = 1;
 }
 
-void __interrupt() ISR(void)
+void __interrupt(irq(IOC), base(8)) IOC_ISR(void)
 {
-    if(PIR0bits.IOCIF)
+    if(IOCCFbits.IOCCF2)
     {
-        if(IOCCFbits.IOCCF1)
+        haltRequest = 1;
+        IOCCFbits.IOCCF2 = 0;
+    }
+
+    PIR0bits.IOCIF = 0;
+}
+
+void Halt_10s(void)
+{
+    unsigned char i;
+
+    LCD_SetCursor(1, 0);
+    LCD_String("SYSTEM HALTED  ");
+
+    LCD_SetCursor(2, 0);
+    LCD_String("ADC paused 10s ");
+
+    for(i = 0; i < 20; i++)
+    {
+        RED_LED = 1;
+        delay_ms(250);
+        RED_LED = 0;
+        delay_ms(250);
+    }
+
+    RED_LED = 0;
+    haltRequest = 0;
+    LCD_Clear();
+}
+
+void main()
+{
+    unsigned int adc;
+    unsigned int voltage_mv;
+    unsigned int last_voltage = 0;
+    unsigned int change;
+
+    int accel_x100;
+
+    char line[17];
+    const char *state;
+
+    OSCCON1 = 0x60;
+    OSCFRQ = 0x02;
+
+    ANSELB = 0x00;
+    ANSELD = 0x00;
+    ANSELC = 0x00;
+    ANSELE = 0x00;
+
+    TRISB = 0x00;
+    TRISD = 0x00;
+
+    LATB = 0x00;
+    LATD = 0x00;
+    LATE = 0x00;
+
+    LCD_Init();
+    ADC_Init();
+    IOC_Init();
+
+    LCD_Clear();
+
+    while(1)
+    {
+        if(haltRequest)
         {
-            IOCCFbits.IOCCF1 = 0;
-            PIR0bits.IOCIF = 0;
-
-            // HALT: blink LED for ~10 sec
-            for(int i = 0; i < 50; i++)
-            {
-                LATEbits.LATE0 = 1;
-                MSdelay(100);
-                LATEbits.LATE0 = 0;
-                MSdelay(100);
-            }
+            haltRequest = 0;
+            Halt_10s();
         }
+
+        adc = ADC_Read();
+
+        voltage_mv = ((unsigned long)adc * 5000UL) / 1023UL;
+
+        if(voltage_mv > last_voltage)
+            change = voltage_mv - last_voltage;
+        else
+            change = last_voltage - voltage_mv;
+
+        if(voltage_mv <= 8000)
+        {
+            accel_x100 = -981;
+        }
+        else if(voltage_mv >= 12500)
+        {
+            accel_x100 = 981;
+        }
+        else if(voltage_mv < 10080)
+        {
+            accel_x100 = -((long)(10080 - voltage_mv) * 981) / (10080 - 8000);
+        }
+        else
+        {
+            accel_x100 = ((long)(voltage_mv - 10080) * 981) / (12500 - 10080);
+        }
+
+        if(change > 500)
+            state = "Shake";
+        else if(voltage_mv >= 11000)
+            state = "Tilt_Right";
+        else if(voltage_mv <= 9700)
+            state = "Tilt_Left";
+        else
+            state = "Flat";
+
+        LCD_SetCursor(1, 0);
+        sprintf(line, "%-16s", state);
+        LCD_String(line);
+
+        LCD_SetCursor(2, 0);
+
+        if(accel_x100 < 0)
+        {
+            sprintf(line, "-%d.%02d m/s2   ",
+                    (-accel_x100) / 100,
+                    (-accel_x100) % 100);
+        }
+        else
+        {
+            sprintf(line, " %d.%02d m/s2   ",
+                    accel_x100 / 100,
+                    accel_x100 % 100);
+        }
+
+        LCD_String(line);
+
+        last_voltage = voltage_mv;
+
+        delay_ms(300);
     }
-}
-
-//-------------------- LCD --------------------
-void LCD_Init()
-{
-    MSdelay(15);
-    LCD_Port = 0x00;
-    LCD_Control = 0x00;
-
-    LCD_Command(0x01);
-    LCD_Command(0x38);
-    LCD_Command(0x0C);
-    LCD_Command(0x06);
-}
-
-void LCD_Clear()
-{
-    LCD_Command(0x01);
-}
-
-void LCD_Command(char cmd)
-{
-    ldata = cmd;
-    RS = 0;
-    EN = 1;
-    NOP();
-    EN = 0;
-    MSdelay(3);
-}
-
-void LCD_Char(char dat)
-{
-    ldata = dat;
-    RS = 1;
-    EN = 1;
-    NOP();
-    EN = 0;
-    MSdelay(1);
-}
-
-void LCD_String(const char *msg)
-{
-    while(*msg)
-    {
-        LCD_Char(*msg++);
-    }
-}
-
-void LCD_String_xy(char row, char pos, const char *msg)
-{
-    char location;
-
-    if(row <= 1)
-        location = 0x80 | (pos & 0x0F);
-    else
-        location = 0xC0 | (pos & 0x0F);
-
-    LCD_Command(location);
-    LCD_String(msg);
-}
-
-//-------------------- DELAY --------------------
-void MSdelay(unsigned int val)
-{
-    unsigned int i, j;
-    for(i = 0; i < val; i++)
-        for(j = 0; j < 165; j++);
 }
